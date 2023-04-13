d6e1ec98440e445a282cab9a5f3f21ad3ea70de7
s222309556 <s222309556@deakin.edu.au>
2023-04-13 21:41:26 +1000

    database connection

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,22 +1,23 @@
-const cardList = [
-    {
-        title: "Kitten 2",
-        image: "images/kitten-2.jpg",
-        link: "About Kitten 2",
-        desciption: "Demo desciption about kitten 2"
+// const cardList = [
+//     {
+//         title: "Kitten 2",
+//         image: "images/kitten-2.jpg",
+//         link: "About Kitten 2",
+//         desciption: "Demo desciption about kitten 2"
 
-    },
-    {
-        title: "Kitten 3",
-        image: "images/kitten-3.jpg",
-        link: "About Kitten 3",
-        desciption: "Demo desciption about kitten 3"
-    }
-]
+//     },
+//     {
+//         title: "Kitten 3",
+//         image: "images/kitten-3.jpg",
+//         link: "About Kitten 3",
+//         desciption: "Demo desciption about kitten 3"
+//     }
+// ]
+
 
 // make get request to the server to recieve the data
-const getProjects = () => {
-    $.get('/api/projects', (response) => {
+const getCats = () => {
+    $.get('/api/cats', (response) => {
         if(response.statusCode==200){
             addCards(response.data);
         }
@@ -31,24 +32,44 @@ const clickMe = () => {
 
 const submitForm = () => {
     let formData = {};
-    formData.first_name = $('#first_name').val();
-    formData.last_name = $('#last_name').val();
-    formData.password = $('#password').val();
-    formData.email = $('#email').val();
+    formData.title = $('#title').val();
+    formData.image = $('#image').val();
+    formData.link = $('#link').val();
+    formData.description = $('#description').val();
+    
     console.log("Form Data Submitted: ", formData);
+    // tell the server to save this to DB
+    try{
+        addCat(formData);
+    }catch(e){
+        console.error(e);
+    }
+
+}
+
+const addCat = (cat) => {
+    $.ajax({
+        url: '/api/cats',
+        data: cat,
+        type:'POST',
+        success: (result) => {
+            alert(result.message);
+            location.reload();
+        }  
+    });
 }
 
 const addCards = (items) => {
     items.forEach(item => {
-        let itemToAppend = '<div class="col s4 center-align">' +
-            '<div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="' + item.image + '">' +
-            '</div><div class="card-content">' +
-            '<span class="card-title activator grey-text text-darken-4">' + item.title + '<i class="material-icons right">more_vert</i></span><p><a href="#">' + item.link + '</a></p></div>' +
-            '<div class="card-reveal">' +
-            '<span class="card-title grey-text text-darken-4">' + item.title + '<i class="material-icons right">close</i></span>' +
-            '<p class="card-text">' + item.desciption + '</p>' +
-            '</div></div></div>';
-        $("#card-section").append(itemToAppend)
+        let itemToAppend = '<div class="col s4 center-align">'+
+    '<div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="'+item.image+'">'+
+    '</div><div class="card-content">'+
+    '<span class="card-title activator grey-text text-darken-4">'+item.title+'<i class="material-icons right">more_vert</i></span><p><a href="#">'+item.link+'</a></p></div>'+
+    '<div class="card-reveal">'+
+        '<span class="card-title grey-text text-darken-4">'+item.title+'<i class="material-icons right">close</i></span>'+
+        '<p class="card-text">'+item.description+'</p>'+
+      '</div></div></div>';
+      $("#card-section").append(itemToAppend)
     });
 }
 
@@ -58,9 +79,8 @@ $(document).ready(function () {
     $('#formSubmit').click(() => {
         submitForm();
     })
-
-    // call getProjects function
-    getProjects();
-    addCards(cardList);
+    // call getCats function
+    getCats();
+    // addCards(cardList);
     $('.modal').modal();
 });
--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 var express = require("express")
 var app = express()
 
+// mongodb connection
+const{MongoClient} = require('mongodb');
+// add database connections
+const uri = 'mongodb+srv://admin:admin@cluster0.3lflt16.mongodb.net/?retryWrites=true&w=majority';
+const client = new MongoClient(uri);
+let dbCollection;
+
 app.use(express.static(__dirname+'/public'))
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -10,23 +17,64 @@ const cardList = [
         title: "Kitten 2",
         image: "images/kitten-2.jpg",
         link: "About Kitten 2",
-        desciption: "Demo desciption about kitten 2"
+        desciption: "Demo description about kitten 2"
 
     },
     {
         title: "Kitten 3",
         image: "images/kitten-3.jpg",
         link: "About Kitten 3",
-        desciption: "Demo desciption about kitten 3"
+        desciption: "Demo description about kitten 3"
     }
 ]
 
-app.get('/api/projects',(req,res) => {
-    res.json({statusCode: 200, data: cardList, message:"Success"})
+app.get('/api/cats',(req,res) => {
+    getAllCats((err, result) => {
+        if(err){
+            res.json({statusCode: 400, message: err});
+        } else {
+            res.json({statusCode: 200, data: result, message:'Successful'});
+        }
+    });
 })
+
+
+
+function dbConnection(collectionName){
+    client.connect(err => {
+        dbCollection = client.db().collection(collectionName);
+        if(!err){
+            console.log('DB Connected');
+            console.log(dbCollection);
+        } else{
+            console.error(err);
+        }
+    });
+}
+
+// post api
+app.post('/api/cats', (req, res) => {
+    let cat = req.body;
+    insert(cat, (err, result) =>{
+        if(err){
+            res.json({statusCode: 400, message: err});
+        } else {
+            res.json({statusCode: 200, data: result, message:'Cat Successfully Added'});
+        }
+    });
+});
+
+function insert(cat, callback){
+    dbCollection.insertOne(cat, callback);
+}
+
+function getAllCats(callback){
+    dbCollection.find().toArray(callback);
+}
 
 var port = process.env.port || 3000;
 
 app.listen(port,()=>{
-    console.log("App listening to: "+port)
+    console.log("App listening to: "+port);
+    dbConnection('Cats');
 })
